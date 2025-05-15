031f1302909c40c9459cb8a7a10aa86ba0c145b4
JamieHart46 <jamiehart31@gmail.com>
2025-05-15 10:50:52 +0200

    gradient background homepage

---
+++ scripts/gradient.js
@@ -0,0 +1,12 @@
+const gradient = document.getElementById('gradient');
+
+document.addEventListener('mousemove', (e) => {
+  const x = e.clientX / window.innerWidth * 100;  // Percentage based on window width
+  const y = e.clientY / window.innerHeight * 100; // Percentage based on window height
+
+  // Set gradient origin based on cursor
+  gradient.style.background = `radial-gradient(ellipse farthest-corner at ${x}% ${y}%, 
+    var(--colorPearlPink) 25%, 
+    var(--colorPearlBlue) 60%, 
+    var(--colorHighlightWhite))`;
+});
