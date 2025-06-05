ed4fc0c4cac6c2f55212d39415b9bc9ec1ed2569
danji45 <118186664+danji45@users.noreply.github.com>
2025-06-05 13:35:30 +0200

    Print sheet, alleen nog geen opdrachten

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ import '../server/scripts/gradient.js';
 // import '../server/scripts/index.js';
 import '../server/scripts/mouse.js';
 import '../server/scripts/accesibility.js';
+import '../server/scripts/detail.js';
 
 console.log('Hello, world!');
 
