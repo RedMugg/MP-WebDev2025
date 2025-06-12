435684ddf35403ad494c2da28f38a337262b20d2
Lisa <lisamandemakers@hotmail.com>
2025-06-12 13:40:23 +0200

    added generative art

--- client/index.js
+++ client/index.js
@@ -4,4 +4,5 @@ import '../server/scripts/gradient.js';
 import '../server/scripts/mouse.js';
 import '../server/scripts/accesibility.js';
 import '../server/scripts/detail.js';
+import '../server/scripts/generativeArt.js';
 
